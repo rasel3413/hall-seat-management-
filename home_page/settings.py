1c4a326e7fb2d12ea387e89b98680474f8998936
Ahsan Habib Rasel <jucse28.367@gmail.com>
2023-04-04 18:40:57 +0600

    updated

--- home_page/settings.py
+++ home_page/settings.py
@@ -122,6 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / "home/static"]
-STATIC_ROOT = BASE_DIR / "static"
