008135fc08b5021ad93d47f9434b7dc11e03f93e
Ahsan Habib Rasel <jucse28.367@gmail.com>
2023-04-04 15:44:38 +0600

    update

--- home_page/settings.py
+++ home_page/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qs_ji64vheiftynmwserco4(ghsq#(wj@r9#n-4vl2kcp&^o5r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
