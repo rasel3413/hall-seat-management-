2e0cdd2e055dba741d6df0dd2540abe4566ee762
Ahsan Habib Rasel <jucse28.367@gmail.com>
2023-04-04 16:20:01 +0600

    updated:

--- home_page/settings.py
+++ home_page/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-qs_ji64vheiftynmwserco4(ghsq#(wj@r9#n-4vl2kcp&^o5r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -38,9 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.FrontpageConfig',
-]
-STATICFILES_DIRS=[
-    BASE_DIR,"home"
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS=[
+    BASE_DIR,"home"
+]
+
